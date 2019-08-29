819f29ef1f5ac119a204ef2661ce9c62342193c6
JamesDream <1741704351@qq.com>
2019-08-29 23:49:51 +0800

    update

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import json
 import config
 
 con = config.config
-url = 'https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?period_id=1HRS&time_start=2016-12-31T00:00:00&time_end=2017-01-01T00:00:00&limit=8760'
+url = 'https://rest.coinapi.io/v1/ohlcv/BITSTAMP_SPOT_BTC_USD/history?period_id=1HRS&time_start=2013-02-24T06:00:00&time_end=2013-02-24T07:00:00&limit=8760'
 headers = {'X-CoinAPI-Key' : con['key']}
 response = requests.get(url, headers=headers)
 json_str = json.dumps(response.json())
